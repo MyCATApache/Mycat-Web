cc270b912f56bdbb24bc19caa1075ad17e5e7bb0
accphuangxin <xin.huang@LT-279.howbuy.domain>
2015-12-05 16:47:37 +0800

    去掉jmx中生成jrds配置文件功能

--- src/main/java/org/mycat/web/service/JMXService.java
+++ src/main/java/org/mycat/web/service/JMXService.java
@@ -43,12 +43,12 @@ public class JMXService extends BaseService {
 	public RainbowContext insert(RainbowContext context) {
 		String guid=new ObjectId().toString();
 		try{
-			String jrdsconfg = buildJrdsPath(context, guid);
+			//String jrdsconfg = buildJrdsPath(context, guid);
 			
 			ZookeeperService.getInstance().insertJmx(guid,context.getAttr());			
 			context.setMsg("新增成功!");
 			context.setSuccess(true);
-			createjmxjrds(jrdsconfg, context.getAttr());
+			//createjmxjrds(jrdsconfg, context.getAttr());
 		}catch (Exception e) {
 			logger.error(e.getCause());
 			context.setSuccess(false);
@@ -82,11 +82,11 @@ public class JMXService extends BaseService {
 			if(jrdsfile != null && !jrdsfile.isEmpty()){
 				new File(jrdsfile).delete();
 			}
-			String jrdsconfg = buildJrdsPath(context,guid);
+			//String jrdsconfg = buildJrdsPath(context,guid);
 			ZookeeperService.getInstance().insertJmx(guid,context.getAttr());		
 			context.setMsg("更新成功!");
 			context.setSuccess(true);
-			createjmxjrds(jrdsconfg, context.getAttr());			
+			//createjmxjrds(jrdsconfg, context.getAttr());			
 		} catch (Exception e) {
 			logger.error("execute error, {} /r/n cause:{}", e.getMessage(), e.getCause());
 			context.setSuccess(Boolean.FALSE);
