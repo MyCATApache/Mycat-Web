ebf019de5bf22eb9ab8021dc0a6f58a3076c5c52
chenshengzun <chenshengzun>
2016-01-07 13:05:42 +0800

    mycat数据持久化，添加时间查询条件

---
+++ src/main/webapp/WEB-INF/db/mycat-web.sql
@@ -0,0 +1,32 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : MySQL3306
+Source Server Version : 50621
+Source Host           : localhost:3306
+Source Database       : mycat-web
+
+Target Server Type    : MYSQL
+Target Server Version : 50621
+File Encoding         : 65001
+
+Date: 2016-01-07 11:13:46
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for t_mycat_sysparam
+-- ----------------------------
+DROP TABLE IF EXISTS `t_mycat_sysparam`;
+CREATE TABLE `t_mycat_sysparam` (
+  `ID` int(11) NOT NULL DEFAULT '0',
+  `START_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `EXECUTE_TIME` int(11) NOT NULL,
+  `USER` varchar(25) NOT NULL,
+  `CONTENT` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- ----------------------------
+-- Records of t_mycat_sysparam
+-- ----------------------------
