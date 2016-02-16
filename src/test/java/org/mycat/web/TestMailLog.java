a18bfc3738ca8096af52fad6f27d207ab0ce28ff
whyuan <whyuan@psh.com.cn>
2016-02-16 15:03:25 +0800

    Mail log机能追加

---
+++ src/test/java/org/mycat/web/TestMailLog.java
@@ -0,0 +1,16 @@
+package org.mycat.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class TestMailLog {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestMailLog.class);
+    
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        LOG.error(" test ");
+        LOG.error("Mail");
+	}
+
+}
