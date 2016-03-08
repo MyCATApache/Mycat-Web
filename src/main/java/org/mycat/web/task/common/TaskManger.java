1aa11384833961acee2c92d0fd7fd8149577545d
whyuan <whyuan@psh.com.cn>
2016-03-08 15:29:07 +0800

    假死监测事件完善。假死DAO层需要修改。

--- src/main/java/org/mycat/web/task/common/TaskManger.java
+++ src/main/java/org/mycat/web/task/common/TaskManger.java
@@ -40,7 +40,19 @@ public class TaskManger {
 	} 
 	
 	public void addTask(final ITask task, long period, String taskName){
-		addTask(task, period, taskName,-1);
+		ScheduledFuture<?> sf = scheduler.scheduleAtFixedRate(new Runnable() {  
+            public void run() {
+            	Date nowDate = new Date();
+        		for(String dbName : dbNames){
+        			try{
+        				task.excute(dbName, nowDate);
+        			}catch(Exception ex){
+                		ex.printStackTrace();
+                	}
+        		}
+            }  
+        }, 0, period, TimeUnit.MILLISECONDS);  
+		taskPool.put(taskName, sf);
 	}
 
 	public void cancelTask(String... taskNames){
@@ -60,39 +72,44 @@ public class TaskManger {
 		}
 	}
 	
-	public void addTask(final ITask task, long period, String taskName,int timeout){
-		
+	public void addTask(final ITask task,final long period, String taskName,final int timeout){
+		//间隔一定时间，触发一次监测事件
 		ScheduledFuture<?> sf = scheduler.scheduleAtFixedRate(new Runnable() {  
             public void run() {
-            	Date nowDate = new Date();
-        		for(String dbName : dbNames){
+            	//一次监测事件，根据DB数量，一个DB，开一个线程监测
+				final Date nowDate = new Date();
+        		for(final String dbName : dbNames){
         			try{
-        				task.excute(dbName, nowDate);
+		        		ScheduledFuture<?> sf = scheduler.schedule(new Runnable() {  
+		        			public void run() {
+		                		task.excute(dbName, nowDate);		
+		        			}
+		        		}, 0, TimeUnit.MILLISECONDS);  
+		        		try {
+		        			if (timeout != -1)
+		        				sf.get(timeout, TimeUnit.SECONDS);
+		        		} catch (TimeoutException e) {
+		        			//主线程等待计算结果超时，因此中断任务线程！
+		        			e.printStackTrace();
+		        			if (task instanceof CheckMycatSuspend){
+		        				//输出Log
+		        				LOG.error("DB:"+((CheckMycatSuspend)task).getDbName()+" 出现假死。");				
+		        			}
+		        		} catch (InterruptedException e) {
+		        			// TODO Auto-generated catch block
+		        			e.printStackTrace();
+		        		} catch (ExecutionException e) {
+		        			// TODO Auto-generated catch block
+		        			e.printStackTrace();
+		        		}
         			}catch(Exception ex){
                 		ex.printStackTrace();
                 	}
         		}
+            	
             }  
         }, 0, period, TimeUnit.MILLISECONDS);  
-		try {
-			if (timeout != -1)
-				sf.get(timeout, TimeUnit.SECONDS);
-		} catch (TimeoutException e) {
-			//主线程等待计算结果超时，因此中断任务线程！
-			e.printStackTrace();
-			if (task instanceof CheckMycatSuspend){
-				//输出Log
-				LOG.error("DB:"+((CheckMycatSuspend)task).getDbName()+" 出现假死。");				
-			}
-			
-			
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 		taskPool.put(taskName, sf);
 	}
 	
